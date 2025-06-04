d001360e02e972a7488aea487c2b504797f7579c
mohitkiloi <mohit.worrk@gmail.com>
2025-06-05 01:13:48 +0200

    Update gnn_model.py

--- gnn/gnn_model.py
+++ gnn/gnn_model.py
@@ -71,4 +71,4 @@ for epoch in range(100):
 
 # Save model
 torch.save(model.state_dict(), "gnn_model.pth")
-print("[✓] Trained GNN model and saved as gnn_model.pth")
+print(" Trained GNN model and saved as gnn_model.pth")
