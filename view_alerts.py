1a2b16c54ba57a5cef6b6f2503f22bd3a20bb410
mohitkiloi <mohit.worrk@gmail.com>
2025-06-05 01:12:09 +0200

    Update view_alerts.py

--- view_alerts.py
+++ view_alerts.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 df = pd.read_csv("alert_report.csv")
 
-print("\n🛡️ Suspicious Login Alerts:")
+print("\n Suspicious Login Alerts:")
 print(df[["timestamp", "email", "ip", "reason", "score"]].tail())
